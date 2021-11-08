c7134700665d3818c927dc9a728d7b7590a92c7d
MKellman26 <57738618+MKellman26@users.noreply.github.com>
2021-11-08 13:20:55 -0500

    Added code for exercise 5
    Exercise 5 - Display Cities in a Country.

--- world.js
+++ world.js
@@ -2,9 +2,10 @@
 "use strict";
 
 window.onload = function(){
-    var lookup_button = document.getElementById("lookup");
+    var lookup_country_button = document.getElementById("lookupCountries");
+    var lookup_cities_button = document.getElementById("lookupCities");
 
-    lookup_button.addEventListener("click", function(e){
+    lookup_country_button.addEventListener("click", function(e){
 
         const country = document.getElementsByTagName("input")[0];
         var xhttp = new XMLHttpRequest(); //create XMLHttpRequest object
@@ -14,7 +15,23 @@ window.onload = function(){
              }
         };
 
-        var url = "world.php?" + "country=" + String(country.value);
+        var url = "world.php?" + "country=" + String(country.value) + "&context=";
+
+        xhttp.open("GET", url); //specify the request type and where file is located in this case the file is in the same folder
+        xhttp.send(); //send php the character to find
+    });
+
+    lookup_cities_button.addEventListener("click", function(e){
+
+        const country = document.getElementsByTagName("input")[0];
+        var xhttp = new XMLHttpRequest(); //create XMLHttpRequest object
+        xhttp.onreadystatechange = function(){
+             if (this.readyState == 4 && this.status == 200) {
+               document.getElementById("result").innerHTML = this.responseText; //display results in the result section
+             }
+        };
+
+        var url = "world.php?" + "country=" + String(country.value) + "&context=countries";
 
         xhttp.open("GET", url); //specify the request type and where file is located in this case the file is in the same folder
         xhttp.send(); //send php the character to find
--- world.php
+++ world.php
@@ -1,31 +1,61 @@
 <?php
     $country = strip_tags($_GET['country']);
+    $context = strip_tags($_GET['context']);
 
     $host = 'localhost';
     $username = 'lab5_user';
     $password = 'password123';
     $dbname = 'world';
 
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    if ($context === ""):
+        $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+        $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 
 ?>
 <table>
   <tr>
-    <th>Name</th>
-    <th>Continent</th>
-    <th>Independence</th>
-    <th>Head of State</th>
+    <th class="cntr">Name</th>
+    <th class="cntr">Continent</th>
+    <th class="cntr">Independence</th>
+    <th class="cntr">Head of State</th>
   </tr>
   <?php foreach ($results as $row): ?>
   <tr>
-    <td><?= $row['name'] ?></td>
-    <td><?= $row['continent'] ?></td>
-    <td><?= $row['independence_year'] ?></td>
-    <td><?= $row['head_of_state'] ?></td>
+    <td class="cntr"><?= $row['name'] ?></td>
+    <td class="cntr"><?= $row['continent'] ?></td>
+    <td class="cntr"><?= $row['independence_year'] ?></td>
+    <td class="cntr"><?= $row['head_of_state'] ?></td>
   </tr>
   <?php endforeach; ?>
 </table>
 
+<?php
+    else:
+        $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+        $stmt = $conn->query("SELECT cities.name, district, cities.population as population FROM countries join cities WHERE countries.name LIKE '%$country%' and code = country_code");
+
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<table>
+  <tr>
+    <th class="city">Name</th>
+    <th class="city">District</th>
+    <th class="city">Population</th>
+  </tr>
+  <?php foreach ($results as $row): ?>
+  <tr>
+    <td class="city"><?= $row['name'] ?></td>
+    <td class="city"><?= $row['district'] ?></td>
+    <td class="city"><?= $row['population'] ?></td>
+  </tr>
+  <?php endforeach; ?>
+</table>
+
+ <?php
+    endif;
+?>
