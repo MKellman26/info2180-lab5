9492f1b041ed78a8babbe8a55571adeb73bef7fd
MKellman26 <57738618+MKellman26@users.noreply.github.com>
2021-11-08 12:13:58 -0500

    Edited world.js to send country to world.php in exercise 3
    Exercise 3 - Look up any Country

--- world.js
+++ world.js
@@ -1,2 +1,22 @@
 /* 620118070 */
 "use strict";
+
+window.onload = function(){
+    var lookup_button = document.getElementsById("lookup")[0];
+
+    lookup_button.addEventListener("click", function(e){
+
+        const country = document.getElementsByTagName("input")[0];
+        var xhttp = new XMLHttpRequest(); //create XMLHttpRequest object
+        xhttp.onreadystatechange = function(){
+             if (this.readyState == 4 && this.status == 200) {
+               document.getElementById("result").innerHTML = this.responseText; //display results in the result section
+             }
+        };
+
+        var url = "world.php?" + "country=" + String(country.value);
+
+        xhttp.open("GET", url); //specify the request type and where file is located in this case the file is in the same folder
+        xhttp.send(); //send php the character to find
+    });
+}
